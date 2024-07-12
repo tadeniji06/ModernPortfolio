2cebe1b9ddf8b653ed666b255c5cf59736452e53
Prince-z3ro <111677193+tadeniji06@users.noreply.github.com>
2024-07-12 13:29:04 +0100

    /

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -47,7 +47,7 @@ const Footer = () => {
               key={info.id}
               className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'
             >
-              <a href={info.link}>
+              <a href={info.link} target="_blank" noreferrer>
                 <img
                   src={info.img}
                   alt='icons'
